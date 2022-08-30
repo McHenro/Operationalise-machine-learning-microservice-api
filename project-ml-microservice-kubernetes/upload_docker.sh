57af65a338b33c734987900626729778823c66a3
Henry <adolphosita@yahoo.com>
2022-08-30 23:54:21 +0100

    updated from make prediction file to upload docker

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=henry557/flaskapp:v8
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag  henry557/flaskapp:v8  henry557/flaskapp:v9
+docker login -u henry557 -p $password
 # Step 3:
 # Push image to a docker repository
+docker push henry557/flaskapp:v9
